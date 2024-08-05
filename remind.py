c469b16fea576cc2e83b2de024b63e4f190f1bf6
zbv-s <aleksandra.jargalova@gmail.com>
2024-08-06 04:45:16 +0800

    Функция check

--- remind.py
+++ remind.py
@@ -5,7 +5,10 @@ import datetime
 import time
 import pygame
 
+t=0
+
 def set():
+    global t
     rem = sd.askstring("Время напоминания", "Введите время напоминания в формате ЧЧ:ММ (в 24 часовом формате)")
     if rem:
         try:
@@ -16,6 +19,16 @@ def set():
             t = dt.timestamp()
         except Exception as e:
             mb.showerror("Ошибка!", f"Произошла ошибка {e}")
+
+
+def check():
+    global t
+    if t:
+        now = time.time()
+        if now >= t:
+            play_snd()
+            t = 0
+    window.after(10000, check)
 
 window = Tk()
 window.title("Напоминание")
