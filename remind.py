884694b21705bc9c82832992f8656b392e28bec7
zbv-s <aleksandra.jargalova@gmail.com>
2024-08-06 04:40:16 +0800

    Функция установки времени

--- remind.py
+++ remind.py
@@ -5,11 +5,23 @@ import datetime
 import time
 import pygame
 
+def set():
+    rem = sd.askstring("Время напоминания", "Введите время напоминания в формате ЧЧ:ММ (в 24 часовом формате)")
+    if rem:
+        try:
+            hour = int(rem.split(":")[0])
+            minute = int(rem.split(":")[1])
+            now = datetime.datetime.now()
+            dt = now.replace(hour=hour, minute=minute)
+            t = dt.timestamp()
+        except Exception as e:
+            mb.showerror("Ошибка!", f"Произошла ошибка {e}")
+
 window = Tk()
 window.title("Напоминание")
 label = Label(text="Установите напоминание")
 label.pack(pady=10)
-set_button =Button(text="Установить напоминание", command=set)
+set_button = Button(text="Установить напоминание", command=set)
 set_button.pack()
 
 window.mainloop()
